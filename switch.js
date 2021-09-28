44171f9d5d22b3400d9b9bb488fdd9593277cc93
Badru <badrufuadalabi@gmail.com>
2021-09-28 23:44:07 +0100

    Adjusted some parts of the html a bit Completed the media query which still needs perfection Removed the unnecessary comments in the Javascript

--- switch.js
+++ switch.js
@@ -1,29 +1,4 @@
-// const metaTags = document.querySelectorAll("meta")
-// metaTags.forEach((meta) => {
-//     if (meta.name == 'theme-color') {
-//         console.log(meta)
-//         meta.setAttribute('content', 'black')
-//         console.log(meta)
-//     }
 
-// })
-// console.log(themeColor); 
-// console.log(upBody);
-
-/*const btn = document.getElementById('swch');
-console.log('btn');
-const upBody = document.getElementsByClassName('whole');
-console.log('upBody');
-const mainBody = document.querySelector('body');
-
-btn.addEventListener('click', () => {
-    mainBody.classList.toggle('whitebody');
-} );
- btn.addEventListener('click', () => {
-     upBody.getElementsByClassName.toggle('wholeTwo');
-     
-});*/
-// localStorage.setItem('theme', 'themeName');
 localStorage.setItem('theme', 'darkTheme');
 localStorage.getItem('theme');
 const btn = document.getElementById('swch');
